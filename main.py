8363aa49e44532456f1fcce174b44e2d42058567
Bruno Cabral <cabral.brc@gmail.com>
2022-06-09 21:38:51 -0700

    add a circle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import arcade
+from arcade.experimental import Shadertoy
 
 
 # Derive an application window from Arcade's parent Window class
@@ -7,9 +8,15 @@ class MyGame(arcade.Window):
         # Call the parent constructor
         super().__init__(width=1920, height=1080)
 
+        # Load a file and create a shader from it
+        file_name = "circle_1.glsl"
+        with open(file_name) as file:
+            shader_source = file.read()
+        self.shadertoy = Shadertoy(size=self.get_size(), main_source=shader_source)
+
     def on_draw(self):
-        # Clear the screen
-        self.clear()
+        # Run the GLSL code
+        self.shadertoy.render()
 
 
 if __name__ == "__main__":
